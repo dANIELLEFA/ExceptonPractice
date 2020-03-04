02f6076dba4a3de31b9ab40b8d1c64bbfc48bc15
danielle.failor <danielle.failor@MU-C1-02.mullen.pvt.k12.co.us>
2020-03-04 12:26:15 -0700

    hi

---
+++ Exceptions/src/geanrh.java
@@ -0,0 +1,16 @@
+
+public class geanrh
+	{
+		
+		public static void main(String[] args)
+			{
+				
+				int subtype = 0;
+				while(true)
+					{
+						
+					}
+
+			}
+
+	}
--- Exceptions/src/practice.java
+++ Exceptions/src/practice.java
@@ -4,8 +4,57 @@ public class practice
 
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				
+//				String[] first = new String[1];
+//				first[0] = "1";
+				exception1();
+				exception2();
+				exception3();
+			}
+		public static void exception1()
+		{
+			//StringIndexOutOfBoundsException
+			String dance = "Dancing Queen";
+			try
+				{
+					int x = 30/4*1+50+9-20;
+					String newDance = dance.substring(x);
+					System.out.println(newDance);
+				}
+			catch(StringIndexOutOfBoundsException exc)
+				{
+					System.out.println("Why would you do that stupid?");
+				}
+		}
+		public static void exception2()
+			{
+				//NullPointerException
+				String cat = null;
+				try
+					{
+					cat = cat.replace(cat.substring(0), "cat");
+						System.out.println(cat + " Zoinks");
+					}
+				catch(NullPointerException exc)
+					{
+						System.out.println("Did you really try that?");
+					}
+				
+			}
+		public static void exception3()
+			{
+				//NumberFormatException
+				String lilly = "dog";
+				try
+					{
+						int weird= Integer.parseInt(lilly);
+						System.out.println(weird);
+						
+					}
+				catch(NumberFormatException exc)
+					{
+						System.out.println("You really should not try that again.");
+					}
 			}
 
 	}
